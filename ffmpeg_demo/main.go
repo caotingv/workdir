9f3826f5a99604c97783aa26b9ac89060741be7d
caotingv <1530692952@qq.com>
2022-11-05 16:02:31 +0800

    Update main.go

--- ffmpeg_demo/main.go
+++ ffmpeg_demo/main.go
@@ -1,20 +1,11 @@
 package main
 
-import (
-	"fmt"
-
-	ffmpeg "github.com/u2takey/ffmpeg-go"
-)
+import "ffmpeg_demo/server"
 
 func main() {
 
-	// server.Start()
+	server.Start()
 	// client.Satrt()
 	// example.ExampleStream("", "", false)
 
-	data, err := ffmpeg.Probe("./file/img1.jpg")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(data)
 }
